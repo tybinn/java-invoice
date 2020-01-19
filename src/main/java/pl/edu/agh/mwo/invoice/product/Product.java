fa8fdfd8dcf7c1d0a9c4d604ef622e1f5ceb9176
student44 <student44@pcoip.ki.agh.edu.pl>
2020-01-19 09:53:46 +0100

    Pierwszy commit

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -16,15 +16,15 @@ public abstract class Product {
 	}
 
 	public String getName() {
-		return null;
+		return name;
 	}
 
 	public BigDecimal getPrice() {
-		return null;
+		return price;
 	}
 
 	public BigDecimal getTaxPercent() {
-		return null;
+		return taxPercent;
 	}
 
 	public BigDecimal getPriceWithTax() {
